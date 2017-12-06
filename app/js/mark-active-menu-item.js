d036cc9c1e28e17c368b269eec6d9ada8be46ab7
Andreas Lymalm <andreas.lymalm@gmail.com>
2017-12-06 21:50:39 +0100

    started with pages korledarna and Magnus

--- app/js/mark-active-menu-item.js
+++ app/js/mark-active-menu-item.js
@@ -11,6 +11,20 @@ if(page == "") {
     link.removeAttribute("href");
     link.parentElement.setAttribute("class", "active");
 } 
+
+// Check if page starts with "korledare".
+else if(page.startsWith("korledare")) {
+    // Find menu item with link to current HTML page.
+    var link = $("nav li > [href='korledarna.html']")[0];
+
+    // Mark active item in menu.
+    link.removeAttribute("href");
+    link.parentElement.setAttribute("class", "active");
+
+    // Make dropdown parent marked active too.
+    link.parentElement.parentElement.parentElement.setAttribute("class", "active");
+}
+
 else {
     // Find menu item with link to current HTML page.
     var link = $("nav li > [href='" + page + "']")[0];
